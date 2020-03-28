8985b0cdc822208128d5e0f92f4a5e541b454f70
Agustín Núñez <seviagus@gmail.com>
2020-03-28 20:05:34 +0100

    Add bash script for hadoop

---
+++ tfidf-start.sh
@@ -0,0 +1,5 @@
+chmod 777 */doc*.py
+
+cat Documents/demo* | ./Job1/mapper.py | sort | ./Job1/reducer.py > outputDemo1.txt
+cat outputDemo1.txt | ./Job2/mapper.py | sort | ./Job2/reducer.py > outputDemo2.txt
+cat outputDemo2.txt | ./Job3/mapper.py | sort | ./Job3/reducer.py > outputFinal.txt
